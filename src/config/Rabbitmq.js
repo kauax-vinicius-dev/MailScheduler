63f8dc0f3fbc25f999bb72e3c71d5abfa5f29d81
kauax-vinicius-dev <kauaxsilva@icloud.com>
2026-08-10 22:25:30 -0300

    feat(Rabbitmq): cria consumer e adiciona tratamento de erros e entradas

--- src/config/Rabbitmq.js
+++ src/config/Rabbitmq.js
@@ -7,21 +7,57 @@ export class RabbitMQ {
     static queueName = 'emailQueue';
 
     static async connect() {
-        this.connection = await amqp.connect('amqp://localhost');
-        this.channel = await this.connection.createChannel();
+        try {
+            this.connection = await amqp.connect('amqp://localhost');
+            this.channel = await this.connection.createChannel();
 
-        await this.channel.assertQueue(this.queueName, {
-            durable: true,
-            arguments: {
-                'x-queue-type': 'quorum'
-            }
-        });
+            await this.channel.assertQueue(this.queueName, {
+                durable: true,
+                arguments: {
+                    'x-queue-type': 'quorum'
+                }
+            });
+
+            console.log("RabbitMQ connected successfully")
+
+        } catch (error) {
+            console.log("Failed to connect to RabbitMQ", error)
+        }
     }
 
-    static async sendToQueue(message) {
+    static sendToQueue(message) {
+        if (!message) {
+            console.log("Message is empty");
+            return;
+        }
+
         this.channel.sendToQueue(this.queueName, Buffer.from(JSON.stringify(message)), {
             persistent: true
         });
+    }
+
+    static consumeFromQueue(callback) {
+        this.channel.consume(this.queueName, async (message) => {
+            if (!message) {
+                console.log("Message is empty")
+                return;
+            }
+
+            try {
+                const content = JSON.parse(
+                    message.content.toString()
+                );
+
+                console.log("Message received:", content);
+
+                await callback(content);
+                this.channel.ack(message);
+
+            } catch (error) {
+                console.error("Error processing message:", error);
+            }
+        });
+
     }
 
 }
