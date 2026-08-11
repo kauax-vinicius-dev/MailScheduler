17d14a23c7c5ee1a198c69872df7a89e48571a5d
kauax-vinicius-dev <kauaxsilva@icloud.com>
2026-08-10 21:26:05 -0300

    refactor(DbConfig): torna connectMongo assíncrona e adiciona await na conexão ao banco

--- src/config/DbConfig.js
+++ src/config/DbConfig.js
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export class Database {
-    static connectMongo() {
+    static async connectMongo() {
         try {
-            mongoose.connect(
+            await mongoose.connect(
                 `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster-1.vxhqtsw.mongodb.net/?appName=Cluster-1`
             )
+            console.log("MongoDB connected successfully.");
         } catch (error) {
             console.error("Error connecting to the database:", error);
         }
