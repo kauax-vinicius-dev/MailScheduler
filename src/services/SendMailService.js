465fd2fe4666ea98c1af4751aa7b2c8cf9a3cf8f
kauax-vinicius-dev <kauaxsilva@icloud.com>
2026-08-14 02:40:07 -0300

    feat(sendMailService): cria classe e função sendMail

---
+++ src/services/SendMailService.js
@@ -0,0 +1,33 @@
+import { EmailService } from "./EmailService.js";
+import { Nodemailer } from "../config/NodemailerConfig.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export class SendMailService {
+    static async sendMail(emailId) {
+
+        if (!emailId) {
+            console.log("Email-id is empty")
+            return;
+        }
+
+        try {
+
+            const email = await EmailService.getMailById(emailId);
+
+            const emailInfo = {
+                from: process.env.SMTP_USER,
+                to: email.recipient,
+                subject: email.subject,
+                text: email.body,
+            }
+
+            await Nodemailer.sendMail(emailInfo);
+            await EmailService.deleteEmail(email.id)
+        } catch (error) {
+            console.error("Error sending email", error)
+        }
+
+    }
+}
