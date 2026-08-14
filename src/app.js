e893072ec70e45334a3dfa51d8edc3ce2f03f285
kauax-vinicius-dev <kauaxsilva@icloud.com>
2026-08-14 03:47:24 -0300

    feat(app): implementa startApplication para orquestração da aplicação

--- src/app.js
+++ src/app.js
@@ -1,30 +1,59 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import routes from './routes/routes.js';
-import path from 'path';
 import cookieParser from 'cookie-parser';
 import { createServer } from 'http';
-import dotenv from "dotenv";
+import routes from './routes/routes.js';
 import { Database } from './config/DbConfig.js';
 import { RabbitMQ } from './config/Rabbitmq.js';
-
+import { Nodemailer } from './config/NodemailerConfig.js';
+import { QueueService } from './services/QueueService.js';
 
 const app = express();
 const port = process.env.PORT;
 const httpServer = createServer(app);
-Database.connectMongo();
-RabbitMQ.connect();
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+
 app.use(routes);
 
 
+async function startApplication() {
+    try {
 
-httpServer.listen(port, () => {
-    console.log(`Server on. PORT:${port}`);
-    console.log(`http://localhost:${port}`)
-});
+        await Database.connectMongo();
+        await RabbitMQ.connect();
+
+        const nodemailerIsValid = await Nodemailer.testConnection();
+
+        if (!nodemailerIsValid) {
+            throw new Error("Nodemailer connection failed");
+        }
+
+        await QueueService.processQueue();
+        QueueService.listeningQueue();
+
+        setInterval(async () => {
+            try {
+                await QueueService.processQueue();
+            } catch (error) {
+                console.error("Error processing queue:", error);
+            }
+        }, 60000);
+
+        httpServer.listen(port, () => {
+            console.log(`Server on. PORT:${port}`);
+            console.log(`http://localhost:${port}`);
+        });
+
+    } catch (error) {
+        console.error("Failed to start application:", error);
+    }
+}
+
+startApplication();
